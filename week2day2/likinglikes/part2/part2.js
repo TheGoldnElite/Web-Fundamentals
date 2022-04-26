586b999e532189d1d0875c4b7a46fc0a6a289380
TheGoldnElite <jsherbstritt@yahoo.com>
2022-04-26 18:57:34 -0400

    commit on main

---
+++ week2day2/likinglikes/part2/part2.js
@@ -0,0 +1,27 @@
+var countElement=document.querySelector("#count");
+var count = 1;
+
+function add(){
+    count++;
+    countElement.innerText = count + " Like(s)";
+    console.log(count);
+}
+
+var count1Element=document.querySelector("#count1");
+var count1 = 1;
+
+function add1(){
+    count1++;
+    count1Element.innerText = count1 + " Like(s)";
+    console.log(count1);
+}
+
+
+var count2Element=document.querySelector("#count2");
+var count2 = 1;
+
+function add2(){
+    count2++;
+    count2Element.innerText = count2 + " Like(s)";
+    console.log(count2);
+}
